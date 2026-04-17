b1467bade5a783852f998d6eb88729b45d43d0f4
Daniel Inostroza Orellana <daynor@gmail.com>
2026-04-17 17:06:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ if st.session_state["vista"] == "inicio":
         
         /* Estilo del botón de entrada (Triángulo) */
         .stButton>button {{
-            position: fixed; top: 35%; left: 35%; width: 30%; height: 30%;
+            position: fixed; top: 40%; left: 35%; width: 30%; height: 30%;
             background: transparent !important; color: transparent !important;
             border: none !important; z-index: 10; cursor: pointer;
             clip-path: polygon(50% 90%, 0 0, 90% 0);
